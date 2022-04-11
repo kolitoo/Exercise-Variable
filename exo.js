72d5c76b8982ef485e76bbf611ff2eee9309afd1
Alex Bourdon <99012539+kolitoo@users.noreply.github.com>
2022-04-11 22:14:58 +0200

    Add files via upload

---
+++ exo.js
@@ -0,0 +1,9 @@
+let vert = "<span style='color:green'> vert </span>",
+  jaune = "<span style='color:yellow'> jaune </span>",
+  rouge = "<span style='color:red'> rouge </span>";
+
+document.write(vert + "-" + jaune + "" + rouge + "<br>");
+
+
+
+
